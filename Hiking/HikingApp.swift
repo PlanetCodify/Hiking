91d923ef3cb5194e8b6011d34cb8b35a4d165ebd
pranavguptachd <pranavguptachd@gmail.com>
2023-09-28 20:16:17 +0530

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by Pranav Gupta on 28/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hiking/Screen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Pranav Gupta on 28/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    CardView()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
